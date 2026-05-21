f7230917e21a8a551595384d58b3da54f3d256ea
Opoku <oceanedeville@hotmail.fr>
2026-05-21 18:34:22 +0200

    Connect frontend to deployed API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://trouve-ton-artisan-zma6.onrender.com/api",
 });
 
 export default api;
